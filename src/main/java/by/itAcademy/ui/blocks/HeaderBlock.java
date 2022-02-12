a08605bb04a9ea7c2052bb06265a56d79ea841b3
User <ilia.tokmurzin@gmail.com>
2022-02-13 01:40:32 +0300

    run test correct commit

--- src/main/java/by/itAcademy/ui/blocks/HeaderBlock.java
+++ src/main/java/by/itAcademy/ui/blocks/HeaderBlock.java
@@ -6,8 +6,6 @@ import by.itAcademy.utils.chromeDriver.Driver;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
--- src/main/java/by/itAcademy/ui/pages/SingUpPage.java
+++ src/main/java/by/itAcademy/ui/pages/SingUpPage.java
@@ -4,11 +4,6 @@ import by.itAcademy.utils.chromeDriver.Driver;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class SingUpPage extends BaseMethodPages {
 
